217c6134682f64bcc2aa3313e4bf132dd5d2fad9
関根慧 <sekine@freedomland.jp>
2020-08-18 21:25:39 +0900

    予算コンポーネントの値をステートで管理し動的に変更するように変更

--- components/Dashboard/components/Budget/Budget.js
+++ components/Dashboard/components/Budget/Budget.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import MoneyIcon from '@material-ui/icons/Money';
 
 const useStyles = makeStyles(theme => ({
@@ -31,20 +33,73 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     alignItems: 'center'
   },
-  differenceIcon: {
+  differenceDownIcon: {
     color: theme.palette.error.dark
   },
-  differenceValue: {
+  differenceDownValue: {
     color: theme.palette.error.dark,
     marginRight: theme.spacing(1)
-  }
+  },
+  differenceUpIcon: {
+    color: theme.palette.success.dark
+  },
+  differenceUpValue: {
+    color: theme.palette.success.dark,
+    marginRight: theme.spacing(1)
+  },
+  differenceForwardIcon: {
+    color: theme.palette.text
+  },
+  differenceForwardValue: {
+    color: theme.palette.text,
+    marginRight: theme.spacing(1)
+  },
 }));
 
+function getBudget() {
+  //予算変動
+  let array = [0, -1, 1, -5, 5, 10, -10];
+  let result = array[Math.floor(Math.random() * array.length)] * 10000;
+  return result;
+}
+function getBudgetPercent(new_budget, budget) {
+  var result = (new_budget - budget) / budget * 100;
+  var n = 2; // 小数点第n位まで残す
+  return Math.floor(result * Math.pow(10, n)) / Math.pow(10, n);
+}
+function ArrowRender({ status, classes}) {
+  if (status === 0 ) {
+    return <ArrowForwardIcon className={classes.differenceForwardIcon} />;
+  } else if (status === 1) {
+    return <ArrowUpwardIcon className={classes.differenceUpIcon} />;
+  } else {
+    return <ArrowDownwardIcon className={classes.differenceDownIcon} />;
+  }
+}
+
 const Budget = props => {
-  const { className, ...rest } = props;
-
+  const {className, ...rest} = props;
   const classes = useStyles();
-
+  const [budget, setBudget] = useState(5300000);
+  const [budgetPercent, setBudgetPercent] = useState(0);
+  const [status, setStatus] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      let new_budget = budget + getBudget()
+      setBudget(new_budget);
+      let percent = getBudgetPercent(new_budget, budget);
+      setBudgetPercent(percent);
+      if (percent === 0) {
+        setStatus(0);
+      } else if (percent > 0) {
+        setStatus(1);
+      } else {
+        setStatus(2);
+      }
+    }, 60000);
+    return () => clearInterval(id);
+  });
+  
   return (
     <Card
       {...rest}
@@ -62,9 +117,9 @@ const Budget = props => {
               gutterBottom
               variant="body2"
             >
-              BUDGET
+              予算
             </Typography>
-            <Typography variant="h3">$24,000</Typography>
+            <Typography variant="h3">{budget.toLocaleString()}円</Typography>
           </Grid>
           <Grid item>
             <Avatar className={classes.avatar}>
@@ -73,12 +128,15 @@ const Budget = props => {
           </Grid>
         </Grid>
         <div className={classes.difference}>
-          <ArrowDownwardIcon className={classes.differenceIcon} />
+          <ArrowRender 
+            status={status}
+            classes={classes}
+          />
           <Typography
-            className={classes.differenceValue}
+            className={status === 0 ? classes.differenceForwardValue : status === 1 ? classes.differenceUpValue : classes.differenceDownValue}
             variant="body2"
           >
-            12%
+            {budgetPercent}%
           </Typography>
           <Typography
             className={classes.caption}
@@ -91,6 +149,7 @@ const Budget = props => {
     </Card>
   );
 };
+
 
 Budget.propTypes = {
   className: PropTypes.string
